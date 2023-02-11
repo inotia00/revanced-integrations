6b21b953d0b1e76577591c766b0f530ebda2b34c
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-02-11 17:06:00 +0700

    Update GeneralAdsPatch.java

--- app/src/main/java/app/revanced/integrations/patches/ads/GeneralAdsPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/GeneralAdsPatch.java
@@ -35,6 +35,7 @@ public final class GeneralAdsPatch extends Filter {
         var officialCard = new BlockRule(SettingsEnum.ADREMOVER_OFFICIAL_CARDS, "official_card");
         var selfSponsor = new BlockRule(SettingsEnum.ADREMOVER_SELF_SPONSOR, "cta_shelf_card");
         var joinMembership = new BlockRule(SettingsEnum.ADREMOVER_CHANNELBAR_JOIN_BUTTON, "compact_sponsor_button");
+        var viewProducts = new BlockRule(SettingsEnum.ADREMOVER_GRAY_SEPARATOR, "product_item", "products_in_video");
         var graySeparator = new BlockRule(SettingsEnum.ADREMOVER_GRAY_SEPARATOR,
                 "cell_divider"
         );
@@ -79,6 +80,7 @@ public final class GeneralAdsPatch extends Filter {
                 communityGuidelines,
                 compactBanner,
                 inFeedSurvey,
+                viewProducts,
                 medicalPanel,
                 merchandise,
                 infoPanel,
