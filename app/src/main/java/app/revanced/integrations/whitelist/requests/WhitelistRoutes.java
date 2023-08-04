532ca5a6077046e412b0051b38412b691a556ae3
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-08-04 11:12:09 +0700

    Add custom api_key field

--- app/src/main/java/app/revanced/integrations/whitelist/requests/WhitelistRoutes.java
+++ app/src/main/java/app/revanced/integrations/whitelist/requests/WhitelistRoutes.java
@@ -1,11 +1,14 @@
 package app.revanced.integrations.whitelist.requests;
 
 import static app.revanced.integrations.requests.Route.Method.GET;
+import app.revanced.integrations.settings.SettingsEnum;
 
 import app.revanced.integrations.requests.Route;
 
 public class WhitelistRoutes {
-    public static final Route GET_CHANNEL_DETAILS = new Route(GET, "videos?part=snippet&key=AIzaSyBYZEMh_dzM31b1zpiHdCRdZAHfUhYFjaE&id={video_id}");
+    private static final String api_key = SettingsEnum.WHITELIST_API_KEY.getString();
+    
+    public static final Route GET_CHANNEL_DETAILS = new Route(GET, "videos?part=snippet&id={video_id}&key=" + api_key);
 
     private WhitelistRoutes() {
     }
