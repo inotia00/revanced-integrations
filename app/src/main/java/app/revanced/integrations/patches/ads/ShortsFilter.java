40af2964e2a3d7bad905aeaf2396a3fc67d66f27
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-09-25 14:43:50 +0700

    fix(youtube/hide-shorts): Shorts shown in search feed

--- app/src/main/java/app/revanced/integrations/patches/ads/ShortsFilter.java
+++ app/src/main/java/app/revanced/integrations/patches/ads/ShortsFilter.java
@@ -20,7 +20,8 @@ public final class ShortsFilter extends Filter {
                 "shorts_shelf",
                 "inline_shorts",
                 "shorts_grid",
-                "shorts_video_cell"
+                "shorts_video_cell",
+                "shorts_pivot_item"
         );
 
         shortsShelfHeader = new StringFilterGroup(
