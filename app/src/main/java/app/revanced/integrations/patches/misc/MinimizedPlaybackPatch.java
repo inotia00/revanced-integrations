11d4776ea8179a649d7e68f11f68f2b39ebc92a5
Hoàng Gia Bảo <70064328+YT-Advanced@users.noreply.github.com>
2023-10-13 20:07:54 +0700

    fix: Disable auto switch to PiP when click External Download Btn

--- app/src/main/java/app/revanced/integrations/patches/misc/MinimizedPlaybackPatch.java
+++ app/src/main/java/app/revanced/integrations/patches/misc/MinimizedPlaybackPatch.java
@@ -1,11 +1,12 @@
 package app.revanced.integrations.patches.misc;
 
 import app.revanced.integrations.shared.PlayerType;
+import static app.revanced.integrations.utils.VideoHelpers.isDownloadBtnClicked;
 
 public class MinimizedPlaybackPatch {
 
     public static boolean isPlaybackNotShort() {
-        return !PlayerType.getCurrent().isNoneOrHidden();
+        return !PlayerType.getCurrent().isNoneOrHidden() && !isDownloadBtnClicked();
     }
 
 }
