c84bee9f676a577b37ae3176b3881d381171b0cf
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-08-11 17:39:55 +0900

    fix(YouTube Music/Sanitize sharing links): tracking parameters are not removed from the system share panel

--- app/src/main/java/app/revanced/integrations/shared/settings/BaseSettings.java
+++ app/src/main/java/app/revanced/integrations/shared/settings/BaseSettings.java
@@ -35,4 +35,6 @@ public class BaseSettings {
     @Deprecated
     // The official ReVanced does not offer this, so it has been removed from the settings only. Users can still access settings through import / export settings.
     public static final StringSetting BYPASS_IMAGE_REGION_RESTRICTIONS_DOMAIN = new StringSetting("revanced_bypass_image_region_restrictions_domain", "yt4.ggpht.com", true);
+
+    public static final BooleanSetting SANITIZE_SHARING_LINKS = new BooleanSetting("revanced_sanitize_sharing_links", TRUE, true);
 }
